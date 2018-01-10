b562f31421ecf88256066e744ff7db94ff7475b4
vladovoev <vladovoev@yahoo.co.uk>
2018-01-10 20:50:59 +0200

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `<div>
+                <h1>{{pageTitle}}</h1>
+                <div>Some Component will be here </div>
+                <pm-products></pm-products>
+            </div>`
+})
+
+export class AppComponent {
+  pageTitle: string = "Acme Product Manager";
+}
